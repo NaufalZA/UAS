871e11923367409a5cc9e191106257f086aea8ac
Zulfan15 <muhammad.zulfan15@mhs.itenas.ac.id>
2023-12-14 12:45:31 +0700

    1
    1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,3 +20,23 @@ Route::get('/', function () {
 });
 
 Route::resource('/product', ProductController::class);
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'post'])->name('login');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::get('/register', [AuthController::class, 'postRegister'])->name('register.store');
+
+Route::group(['middleware' => 'auth'], function(){
+    Route::group(['prefix' => 'product', 'as' => 'product.'], function(){
+        Route::get('/', [ProfileController::class, 'index'])->name('index');
+        Route::get('/create', [ProductController::class, 'create'])->name('create');
+        Route::post('/create', [ProductController::class, 'store'])->name('store');
+        Route::get('/buy/{id}', [ProductController::class, 'buy'])->name('buy');
+       Route::get('/my', [ProductController::class, 'my'])->name('my');
+    });
+ 
+    Route::group(['prefix' => 'profile', 'as' => 'profile.'], function(){
+        Route::get('/', [ProfileController::class, 'index'])->name('index');
+        Route::get('/purchase', [ProfileController::class, 'purchase'])->name('purchase');
+    });
+ 
+});
