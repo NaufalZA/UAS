9507aa562aa261ac4e0f56d7ba3d0459b43731bb
Naufal Zaidan <naufal.zaidan@mhs.itenas.ac.id>
2023-12-16 14:44:11 +0700

    bener

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,6 @@ Route::get('/', function () {
     return view('welcome');
 });
  
- 
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/login', [AuthController::class, 'post'])->name('login');
 Route::get('/register', [AuthController::class, 'register'])->name('register');
