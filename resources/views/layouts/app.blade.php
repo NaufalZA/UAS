8f59700bac817c733153d210e813715efabf5bca
Naufal Zaidan <naufal.zaidan@mhs.itenas.ac.id>
2023-12-17 00:50:54 +0700

    1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,8 +35,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="../../index3.html" class="brand-link">
-      <img src="../assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <img src="../assets/img/Logo.png" alt="RumahMurah Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">RumahMurah</span>
     </a>
  
     <!-- Sidebar -->
@@ -46,13 +46,26 @@
         <div class="image">
           <img src="../assets/img/avatar4.png" class="img-circle elevation-2" alt="User Image">
         </div>
+        <script>
+        window.onload = function() {
+          var nameElement = document.querySelector('.user-name');
+          var name = nameElement.textContent;
+          var words = name.split(' ');
+
+          if (words.length > 2) {
+            words.splice(2, 0, '<br>');
+            nameElement.innerHTML = words.join(' ');
+          }
+        };
+        </script>
+
         <div class="info">
-          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+          <a href="#" class="d-block user-name">{{ Auth::user()->name }}</a>
         </div>
       </div>
  
       <!-- SidebarSearch Form -->
-      <div class="form-inline">
+      <!-- <div class="form-inline">
         <div class="input-group" data-widget="sidebar-search">
           <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
           <div class="input-group-append">
@@ -61,7 +74,7 @@
             </button>
           </div>
         </div>
-      </div>
+      </div> -->
  
       <!-- Sidebar Menu -->
       <nav class="mt-2">
