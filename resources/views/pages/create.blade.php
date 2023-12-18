6a8db884c8a59b5c22d126d337d2dcaffdd0927b
Naufal Zaidan <114813582+NaufalZA@users.noreply.github.com>
2023-12-18 01:49:34 +0000

    dari hp

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -4,31 +4,31 @@
   <div class="container-fluid py-4">
     <div class="row">
       <div class="col-12 col-lg-6">
-        <div class="card">
+        <div class="card"> 
           <div class="card-body p-3">
-            <h5>Add Product</h5> 
+            <h5>Tambah Rumah</h5> 
             <hr>
             <form action="{{ route('product.store') }}" method="POST" enctype="multipart/form-data">
               @csrf
               <div class="form-group">
-                <label for="name" class="form-control-label">Name</label>
+                <label for="name" class="form-control-label">Judul</label>
                 <input type="text" name="name" class="form-control" id="name" required>
               </div>
               <div class="form-group">
-                <label for="image" class="form-control-label">Image</label>
+                <label for="image" class="form-control-label">Gambar</label>
                 <input type="file" name="image" class="form-control form-control-file" id="image" accept=".jpg, .png, .svg" required>
               </div>
               <div class="form-group">
-                <label for="description" class="form-control-label">Description</label>
+                <label for="description" class="form-control-label">Deskripsi</label>
                 <textarea rows="6" name="description" class="form-control" id="description" required></textarea>
               </div>
               <div class="form-group">
-                <label for="price" class="form-control-label">Price</label>
+                <label for="price" class="form-control-label">Harga</label>
                 <input type="number" name="price" class="form-control" id="price" required>
-                <small>* Minimal $. 1</small>
+              
               </div>
               <div class="form-group">
-                <button class="btn btn-primary">Add</button>
+                <button class="btn btn-primary">Tambah</button>
               </div>
             </form>
           </div>
